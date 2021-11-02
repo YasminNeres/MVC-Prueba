ba2436ff296c3b79398d3cc1d88010a4da339c74
Yasmin <neresdesouza.yasmin@gmail.com>
2021-11-02 12:46:25 +0100

    Paso4

---
+++ MVC-Prueba/Data/MVC_PruebaContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MvcMovie.Models;
+
+namespace MVC_Prueba.Data
+{
+    public class MVC_PruebaContext : DbContext
+    {
+        public MVC_PruebaContext (DbContextOptions<MVC_PruebaContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MvcMovie.Models.Movie> Movie { get; set; }
+    }
+}
--- MVC-Prueba/Startup.cs
+++ MVC-Prueba/Startup.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using MVC_Prueba.Data;
 
 namespace MVC_Prueba
 {
@@ -24,6 +26,9 @@ namespace MVC_Prueba
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<MVC_PruebaContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("MVC_PruebaContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
